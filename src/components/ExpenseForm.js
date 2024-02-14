3b59143ed5c50ef9f669afedf8cc8ab97a1ef801
Charon1223 <chlalstn9@gmail.com>
2024-02-14 11:04:10 +0900

    create기능

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,46 @@
+
+import React from 'react'
+import './ExpenseForm.css';
+
+const ExpenseForm = ({ charge, handleCharge, amount, handleAmount, handleSubmit }) => {
+    
+
+
+  
+  return (
+      <form onSubmit={handleSubmit}>
+        <div className='form-center'>
+          <div className='form-group'>
+            <label for='charge'>상품</label>
+            <input
+              type='text'
+              className='form-control'
+              id='charge'
+              name='charge'
+              placeholder='예) 콜라' 
+              value={charge}
+              onChange={handleCharge}
+              />
+          </div>
+
+          <div className='form-group'>
+            <label for='amount'>비용</label>
+            <input
+              type='number'
+              className='form-control'
+              id='amount'
+              name='amount'
+              value={amount}
+              onChange={handleAmount}
+              placeholder='예) 100' />
+          </div>
+        </div>
+        <button type='submit' className='btn'>
+          제출
+        </button>
+      </form>
+    )
+}
+
+
+export default ExpenseForm;
